237915d4b35ef61b82ce9ab63f8c877a389dc519
MRSE435 <m54655967@gmail.com>
2026-06-21 21:03:22 +0530

    added maintanance mode

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Maintenance from "./components/Maintenance";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -17,12 +17,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+    const maintenanceMode = true;
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {maintenanceMode ? <Maintenance /> : children}
+      </body>
     </html>
   );
 }
